ed90c6203fc3c49bfd8471d41154176f3d41beee
Nikita213-hub <70839622+Nikita213-hub@users.noreply.github.com>
2021-07-09 11:09:58 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split( '' );
+  for (let count = 0; count < arr.length; count += 1) {
+  for (let counter = 0; counter < bracketsConfig.length; counter += 1) {
+    if (arr[count] === bracketsConfig[counter][0] && arr[count + 1] === bracketsConfig[counter][1]) {
+              arr.splice(count, 2);
+              count = -1;
+          }
+  }
+  }
+  let res = arr.length ? false : true;
+
+return res;
 }
